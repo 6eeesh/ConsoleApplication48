5b8b179437b224754d6136de383ca9e8b99b860f
6eeesh <144094732+6eeesh@users.noreply.github.com>
2023-12-27 12:26:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication48/ConsoleApplication48.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream> 
+using namespace std;
+
+class Application
+{
+	static Application* instance;
+	string title;
+
+	// private: 
+	Application()
+	{
+		title = "God's Menu";
+	}
+
+public:
+	static Application* GetInstance()
+	{
+		return instance == nullptr ? instance = new Application() : instance;
+	}
+
+	void StartRandomize()
+	{
+		srand(static_cast<unsigned>(time(nullptr)));
+		int random_number = rand() % 100 + 1;
+		cout << "Random number: " << random_number << endl;
+	}
+
+	string GetTitle() const
+	{
+		return title;
+	}
+
+	void SetTitle(string title)
+	{
+		this->title = title;
+	}
+
+};
+
+Application* Application::instance = nullptr;
+
+int main()
+{
+	Application* ptr = Application::GetInstance();
+	cout << ptr->GetTitle() << "\n";
+	ptr->SetTitle("Super Bowl");
+	cout << ptr->GetTitle() << "\n";
+	ptr->StartRandomize();
+}
